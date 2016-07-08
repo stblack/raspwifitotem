67e251c7c8425693c1e8e5f72af5abbebf9577ea
Stefano Negro <stblack@gmail.com>
2016-07-08 18:44:05 +0200

    Update startraspi.sh

--- startraspi.sh
+++ startraspi.sh
@@ -15,8 +15,8 @@ dpkg-reconfigure tzdata
 nano /etc/timezone
 
 #correzione frequenza della posta seriale
-echo"#correzione frequenza della posta seriale" >> /boot/config.txt
-echo"core_freq=250" >> /boot/config.txt
+echo "#correzione frequenza della posta seriale" >> /boot/config.txt
+echo "core_freq=250" >> /boot/config.txt
 
 #eliminare dal file /boot/cmdline.txt la parte  : console=serial0,115200
 sed -i "s/console=serial0,115200//g" /boot/cmdline.txt
@@ -26,7 +26,7 @@ apt-get update
 apt-get dist-upgrade -y
 
 #installo tutti i pacchetti necessari
-sudo apt-get install dnsmasq hostapd putty lighttpd php5 sqlite3 php5-common php5-cgi php5-sqlite mc dnsutils
+apt-get -y install dnsmasq hostapd putty lighttpd php5 sqlite3 php5-common php5-cgi php5-sqlite mc dnsutils
 
 #configurazione webserver
 lighty-enable-mod fastgci
@@ -37,9 +37,20 @@ lighty-enable-mod fastcgi-php
 mv /etc/hosts /etc/hosts.orig
 wget -P /etc/ https://raw.githubusercontent.com/stblack/raspwifitotem/master/etc/hosts
 
-cp /etc/dhcpcd.conf /etc/dhcpcd.conf.orig
-cp /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
-cp /etc/network/interfaces /etc/network/interfaces.orig
-cp /etc/lighttpd.conf /etc/lighttpd.conf.orig
-cp /etc/hostapd/hostapd.conf /etc/hostapd.conf.orig
+#copio i file di configurazione prima di scaricare la configurazione da github
+mv /etc/hosts /etc/hosts.orig
+wget -P /etc/ https://raw.githubusercontent.com/stblack/raspwifitotem/master/etc/hosts
+mv /etc/dhcpcd.conf /etc/dhcpcd.conf.orig
+wget -P /etc/ https://raw.githubusercontent.com/stblack/raspwifitotem/master/etc/dhcpcd.conf
+mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
+wget -P /etc/ https://raw.githubusercontent.com/stblack/raspwifitotem/master/etc/dnsmasq.conf
+mv /etc/network/interfaces /etc/network/interfaces.orig
+wget -P /etc/network https://raw.githubusercontent.com/stblack/raspwifitotem/master/etc/interfaces
+mv /etc/lighttpd.conf /etc/lighttpd.conf.orig
+wget -P /etc/ https://raw.githubusercontent.com/stblack/raspwifitotem/master/etc/lighttpd.conf
+mv /etc/hostapd/hostapd.conf /etc/hostapd.conf.orig
+wget -P /etc/hostapd https://raw.githubusercontent.com/stblack/raspwifitotem/master/hostapd/hostapd.conf
+
+#aggiungere riga per fare partire in automatico come demone hoatapd
+echo "DAEMON_CONF=\"/etc/hostapd/hostapd.conf\"" >> /etc/default/hostapd
 
