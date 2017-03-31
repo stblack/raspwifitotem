a519deb2a43d29f352d8c78691c420d411e3cfbd
Stefano Negro <stblack@gmail.com>
2017-04-01 00:22:37 +0200

    Step correction and URL validation
    Corrected an error in the step variabile assign instructions (spaces)
    and added a function to validate url before to download configuration
    files

--- startraspi.sh
+++ startraspi.sh
@@ -2,11 +2,11 @@
 
 # funzione per controllare se un per corso esiste
 function validate_url(){
-  if [[ `wget -S --spider $1  2>&1 | grep 'HTTP/1.1 200 OK'` ]]; then echo "true"; fi
+  if [[ S(wget -S --spider $1  2>&1 | grep 'HTTP/1.1 200 OK') ]]; then echo "true"; fi
 }
 
 # variabile per contare i passi di configurazione
-step = 1
+step=1
 
 echo -e "\n************************************************************"
 echo "Step $step : inserire in fstab la configurazione per montare tmp in RAM"
@@ -21,25 +21,25 @@ else
         
 fi
 
-step = $(($step+1))
+step=$((step+1))
 echo -e "\n************************************************************"
 echo "Step $step : Configurazione locale"
 dpkg-reconfigure tzdata
 echo "Step $step : Modifica locale eseguita"
 
-step = $(($step+1))
+step=$((step+1))
 echo -e "\n************************************************************"
 echo "Step $step : Codifica fuso orario"
 nano /etc/timezone
 echo "Step $step : Modifica fuso orario eseguita"
 
-step = $(($step+1))
+step=$((step+1))
 echo -e "\n************************************************************"
 echo "Step $step : Configurazione tastiera"
 dpkg-reconfigure keyboard-configuration
 echo "Step $step : Modifica tastiera eseguita"
 
-step = $(($step+1))
+step=$((step+1))
 echo -e "\n************************************************************"
 echo "Step $step : Correzione frequenza della porta seriale"
 if grep -Fxq "core_freq=250" /boot/config.txt
@@ -52,58 +52,76 @@ else
         echo "Step $step : Modifica frequenza della porta seriale eseguita"
 fi
 
-step = $(($step+1))
+step=$((step+1))
 echo -e "\n************************************************************"
 echo "Step $step : Eliminare dal file /boot/cmdline.txt la parte di configurazione della porta console : console=serial0,115200"
 sed -i "s/console=serial0,115200//g" /boot/cmdline.txt
 echo "Step $step : Modifica porta console eseguita"
 
-step = $(($step+1))
+step=$((step+1))
 echo -e "\n************************************************************"
 echo "Step $step : Aggiornamento repository raspberry"
 apt-get update
 apt-get dist-upgrade -y
 echo "Step $step : Aggiornamento repository eseguito"
 
-step = $(($step+1))
+step=$((step+1))
 echo -e "\n************************************************************"
 echo "Installo tutti i pacchetti aggiuntivi necessari : dnsmasq hostapd putty lighttpd php5 sqlite3 php5-common php5-cgi php5-sqlite mc dnsutils"
 apt-get -y install dnsmasq hostapd putty lighttpd php5 sqlite3 php5-common php5-cgi php5-sqlite mc dnsutils
 echo "Step $step : Installazione pacchetti eseguita"
 
-step = $(($step+1))
+step=$((step+1))
 echo -e "\n************************************************************"
 echo "Configurazione webserver PHP"
 lighty-enable-mod fastcgi
 lighty-enable-mod fastcgi-php
 echo "Step $step : Configurazione webserver eseguita"
 
-step = $(($step+1))
+step=$((step+1))
 echo -e "\n************************************************************"
-echo "Copio i file di configurazione attuali e scarico la configurazione da github"
-mv /etc/hosts /etc/hosts.orig
+echo "Backup dei file di configurazione attuali e scarico la configurazione da github online"
 
-if validate_url https://raw.githubusercontent.com/stblack/raspwifitotem/master/etc/hosts then
+if validate_url https://raw.githubusercontent.com/stblack/raspwifitotem/master/etc/hosts; then
+    mv /etc/hosts /etc/hosts.orig
     wget -P /etc/ https://raw.githubusercontent.com/stblack/raspwifitotem/master/etc/hosts
-; fi
-mv /etc/dhcpcd.conf /etc/dhcpcd.conf.orig
-wget -P /etc/ https://raw.githubusercontent.com/stblack/raspwifitotem/master/etc/dhcpcd.conf
-mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
-wget -P /etc/ https://raw.githubusercontent.com/stblack/raspwifitotem/master/etc/dnsmasq.conf
-mv /etc/network/interfaces /etc/network/interfaces.orig
-wget -P /etc/network https://raw.githubusercontent.com/stblack/raspwifitotem/master/etc/network/interfaces
-mv /etc/lighttpd/lighttpd.conf /etc/lighttpd/lighttpd.conf.orig
-wget -P /etc/lighttpd https://raw.githubusercontent.com/stblack/raspwifitotem/master/etc/lighttpd/lighttpd.conf
-mv /etc/hostapd/hostapd.conf /etc/hostapd.conf.orig
-wget -P /etc/hostapd https://raw.githubusercontent.com/stblack/raspwifitotem/master/etc/hostapd/hostapd.conf
-echo "Step $step : scaricamento file configurazione eseguita eseguita"
+fi
 
+if validate_url https://raw.githubusercontent.com/stblack/raspwifitotem/master/etc/dhcpcd.conf; then
+    mv /etc/dhcpcd.conf /etc/dhcpcd.conf.orig
+    wget -P /etc/ https://raw.githubusercontent.com/stblack/raspwifitotem/master/etc/dhcpcd.conf
+fi
+
+if validate_url https://raw.githubusercontent.com/stblack/raspwifitotem/master/etc/dnsmasq.conf; then
+    mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
+    wget -P /etc/ https://raw.githubusercontent.com/stblack/raspwifitotem/master/etc/dnsmasq.conf
+fi
+
+if validate_url https://raw.githubusercontent.com/stblack/raspwifitotem/master/etc/network/interfaces; then
+    mv /etc/network/interfaces /etc/network/interfaces.orig
+    wget -P /etc/network https://raw.githubusercontent.com/stblack/raspwifitotem/master/etc/network/interfaces
+fi
+
+if validate_url https://raw.githubusercontent.com/stblack/raspwifitotem/master/etc/lighttpd/lighttpd.conf; then
+    mv /etc/lighttpd/lighttpd.conf /etc/lighttpd/lighttpd.conf.orig
+    wget -P /etc/lighttpd https://raw.githubusercontent.com/stblack/raspwifitotem/master/etc/lighttpd/lighttpd.conf
+fi
+
+if validate_url https://raw.githubusercontent.com/stblack/raspwifitotem/master/etc/hostapd/hostapd.conf; then
+    mv /etc/hostapd/hostapd.conf /etc/hostapd.conf.orig
+    wget -P /etc/hostapd https://raw.githubusercontent.com/stblack/raspwifitotem/master/etc/hostapd/hostapd.conf
+fi
+
+echo "Step $step : scaricamento file configurazione eseguita"
+step=$((step+1))
 echo -e "\n************************************************************"
-echo "Pagina per bypassare la ricerca del server google in background dentro android 6 "
+echo "Patch pagina per bypassare la ricerca del server google in background dentro android 6 "
 wget -P /var/www/html/ https://raw.githubusercontent.com/stblack/raspwifitotem/master/var/www/html/generate_204.php
 wget -P /var/www/html/ https://raw.githubusercontent.com/stblack/raspwifitotem/master/var/www/html/index.lighttpd.html
 wget -P /var/www/html/ https://raw.githubusercontent.com/stblack/raspwifitotem/master/var/www/html/phpinfo.php
+echo "Step $step : applicazione patch android 6 eseguita"
 
+step=$((step+1))
 echo -e "\n************************************************************"
 echo "Aggiungo riga per fare partire in automatico come demone hostapd"
 echo "DAEMON_CONF=\"/etc/hostapd/hostapd.conf\"" >> /etc/default/hostapd
@@ -115,7 +133,9 @@ else
         # code if not found
         echo "DAEMON_CONF=\"/etc/hostapd/hostapd.conf\"" >> /etc/default/hostapd
 fi
+echo "Step $step : set demone hostapd eseguito"
 
+step=$((step+1))
 echo -e "************************************************************"
 echo "Riavvio finale dei servizi"
 service networking restart
@@ -124,4 +144,5 @@ service dhcpcd restart
 service lighttpd restart
 service hostapd restart
 service dnsmasq restart
-
+echo "Step $step : Fine Configurazione"
+echo -e "************************************************************"
